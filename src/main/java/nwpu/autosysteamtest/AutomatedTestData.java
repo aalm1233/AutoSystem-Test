24e5f7b75c3fa839689a88b2a58ffbcfd2fef146
aalm1233 <369612258@qq.com>
2017-12-12 18:24:14 +0800

    12.12

--- src/main/java/nwpu/autosysteamtest/AutomatedTestData.java
+++ src/main/java/nwpu/autosysteamtest/AutomatedTestData.java
@@ -9,7 +9,7 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class AutomatedTestData {
-	DocumentPrepcessing  documentPrepcessing;
+	DocumentPrepcessing documentPrepcessing;
 	private ConcurrentHashMap<String, String> operaterTypesMap;
 	protected ConcurrentHashMap<String, ArrayList<String>> addInterfaceSetMap;
 	protected ConcurrentHashMap<String, ArrayList<String>> deleteInterfaceSetMap;
@@ -19,8 +19,8 @@ public class AutomatedTestData {
 	protected String resourcesid;
 	protected String resourceid;
 	protected String path;
-	
-	public AutomatedTestData(String path) throws InterruptedException{
+
+	public AutomatedTestData(String path) throws InterruptedException {
 		this.path = path;
 		documentPrepcessing = DocumentPrepcessing.getInstance();
 		this.operaterTypesMap = documentPrepcessing.getOperaterTypesMap();
@@ -31,92 +31,98 @@ public class AutomatedTestData {
 		this.parameterConstrainsMap = documentPrepcessing.getParameterConstrainsMap();
 		this.run1();
 	}
-	protected void run1(){
+
+	protected void run1() {
 		Set<String> key = operaterTypesMap.keySet();
 		File file = null;
-		try{
-			
+		try {
 			file = new File(path + "date");
-			if(!file.exists()){
+			if (!file.exists()) {
 				file.mkdirs();
 			}
-		}catch(Exception e){
-			
-		}finally {
+		} catch (Exception e) {
+
+		} finally {
 			file = null;
 		}
 		this.path = path + "date\\";
-		for(Iterator<String> it = key.iterator(); it.hasNext();){
+		for (Iterator<String> it = key.iterator(); it.hasNext();) {
 			this.resourcesid = (String) it.next();
-			try{
-				file = new File(path+this.resourcesid);
-				if(!file.exists()){
+			try {
+				file = new File(path + this.resourcesid);
+				if (!file.exists()) {
 					file.mkdirs();
 				}
-			}catch(Exception e){
-				
-			}finally {
+			} catch (Exception e) {
+
+			} finally {
 				file = null;
 			}
 			try {
 				run2();
 			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 	}
-	protected void run2() throws FileNotFoundException{
-		xAutomated("add",this.addInterfaceSetMap);
-		xAutomated("update",this.updateInterfaceSetMap);
-		xAutomated("delete",this.deleteInterfaceSetMap);
-		//findDateAutomated();
+
+	protected void run2() throws FileNotFoundException {
+		xAutomated("add", this.addInterfaceSetMap);
+		xAutomated("update", this.updateInterfaceSetMap);
+		xAutomated("delete", this.deleteInterfaceSetMap);
+		// findDateAutomated();
 	}
+
 	private void findDateAutomated() {
 
 	}
-	private void xAutomated(String operation, ConcurrentHashMap<String, ArrayList<String>> xInterfaceSetMap) throws FileNotFoundException {
+
+	private void xAutomated(String operation, ConcurrentHashMap<String, ArrayList<String>> xInterfaceSetMap)
+			throws FileNotFoundException {
 		ArrayList<String> xInterfaceSets = xInterfaceSetMap.get(this.resourcesid);
 		ArrayList<String> parameterConstrains = null;
-		if(this.parameterConstrainsMap.containsKey(resourcesid)){
+		if (this.parameterConstrainsMap.containsKey(resourcesid)) {
 			parameterConstrains = this.parameterConstrainsMap.get(this.resourcesid);
 		}
-		for(String xInterface:xInterfaceSets){
+		for (String xInterface : xInterfaceSets) {
 			this.resourceid = xInterface.split("\\|")[0].split(",")[0];
 			File file = null;
-			try{
-				file = new File(path+this.resourcesid+"\\"+operation+"\\"+this.resourceid);
-				if(!file.exists()){
+			try {
+				file = new File(path + this.resourcesid + "\\" + operation + "\\" + this.resourceid);
+				if (!file.exists()) {
 					file.mkdirs();
 				}
-			}catch(Exception e){
-				
-			}finally {
+			} catch (Exception e) {
+
+			} finally {
 				file = null;
 			}
 			ArrayList<String> resourceParameterConstrains = new ArrayList<>();
-			for(String parameterConstrain:parameterConstrains){
-				if(parameterConstrain.startsWith(resourceid)){
+			for (String parameterConstrain : parameterConstrains) {
+				if (parameterConstrain.startsWith(resourceid)) {
 					resourceParameterConstrains.add(parameterConstrain);
 				}
 			}
-			generateDataFile(this.path+this.resourcesid+"\\"+operation+"\\"+this.resourceid+"\\",xInterface,resourceParameterConstrains);
+			generateDataFile(this.path + this.resourcesid + "\\" + operation + "\\" + this.resourceid + "\\",
+					xInterface, resourceParameterConstrains);
 		}
-		
+
 	}
-	private void generateDataFile(String path,String xInterface,ArrayList<String> parameterConstrains){
+
+	private void generateDataFile(String path, String xInterface, ArrayList<String> parameterConstrains) {
 		System.out.println(xInterface);
 		String[] paramsplt = xInterface.split("->");
-		if(paramsplt.length >1){
+		if (paramsplt.length > 1) {
 			String[] params = paramsplt[1].split("_");
-			for(String param:params){
+			for (String param : params) {
 				String paramName = param.split(",")[0];
 				String paramConstrains = null;
-				for(String s:parameterConstrains){
-					if(s.startsWith(resourceid+"-"+paramName))paramConstrains = s;
+				for (String s : parameterConstrains) {
+					if (s.startsWith(resourceid + "-" + paramName))
+						paramConstrains = s;
 				}
 				try {
-					PrintWriter out = new PrintWriter(path + paramName+".xml");
+					PrintWriter out = new PrintWriter(path + paramName + ".xml");
 					out.println(param);
 					out.flush();
 					out.println(paramConstrains);
@@ -127,7 +133,7 @@ public class AutomatedTestData {
 				}
 			}
 		}
-		
+
 	}
 
 }
